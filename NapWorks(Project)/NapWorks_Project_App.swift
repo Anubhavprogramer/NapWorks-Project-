008de8179effaeffb2848d7abf2635abb646178c
Anubhav <programmingwithanubhav@gmail.com>
2025-10-03 13:14:51 +0530

    Initial Commit

---
+++ NapWorks(Project)/NapWorks_Project_App.swift
@@ -0,0 +1,17 @@
+//
+//  NapWorks_Project_App.swift
+//  NapWorks(Project)
+//
+//  Created by Anubhav Dubey on 03/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NapWorks_Project_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
